1a4d742ceaa6769d2ba15fb85fdfed6e5b10a2ef
juliettesav <juliettesavarese@gmail.com>
2024-03-07 16:37:38 -0600

    daily commit

--- reviewexercises.py
+++ reviewexercises.py
@@ -73,3 +73,95 @@ else:
     print("That is not a valid temperature.")
 
 print("All done!")
+
+# While Loops 
+
+i = 1 
+
+while i <= 1_000: # You can use an underscore to separate digits of a number. 
+    print(i * '*') # This mulitplies the string as many times as the variable i.
+    i = i + 1
+
+# Lists
+names = ["John", "David", "Alexis", "Moira", "Stevie"]
+print(names)
+print(names[0]) # Represents the first item in the list. 
+print(names[-1]) # Represents the last item in the list. 
+
+names[0] = "Johnny"
+print(names)
+
+print(names[0:3])
+
+# List Methods
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+
+numbers.insert(0, -1)
+print(numbers)
+
+numbers.remove(3)
+print(numbers)
+
+print(1 in numbers) # Returns a boolean statement of whether the number is contained in the list. 
+
+print(len(numbers)) # Returns the number of elements in the list. 
+
+numbers.clear() # No values expected. Clears the list.
+print(numbers)
+
+# For Loops (Shorter alternative to While Loops) 
+numbers = [1, 2, 3, 4, 5]
+for item in numbers:
+    print(item) # Prints each item on a new line. 
+
+i = 0 
+while i < len(numbers):
+    print(numbers[i])
+    i = i + 1
+
+# Range() Function 
+num = range(5)
+print(num)
+# Iterate over a range to print the full range rather that the default range format. 
+num = range(5, 10) # Does not include the number 10 but does include the number 5. 
+for number in num:
+    print(number)
+
+num = range(5, 10, 2) # The number 2 signifies the increments. 
+for number in num:
+    print(number)
+
+num = range(5, 10, 2)  
+for number in range(5): # Returns the items within 0 to 4
+    print(number)
+
+# Tuples 
+numbers2 = (1, 2, 3) # Parentheses are used in Tuples and Square Brackets are used for Lists. 
+# Tupples are immutable and unchangable. 
+numbers2.count()
+numbers2.index()
+
+# Types of Data in Python
+print("___Numbers___")
+print(f'Integers: {1, 2, 3}')
+print(f'Floats: {1.1, 1.2, 1.3}')
+
+print('')
+
+print("___Booleans___")
+print(f'{True, False}')
+
+print('')
+
+print("___Strings___")
+print('"Stuff in quotation marks')
+
+print('')
+
+print("___Lists___")
+print(f'{[1, 2, 3]}')
+
+print("___Tuples___")
+print(f'{[1, 2, 3]}')
