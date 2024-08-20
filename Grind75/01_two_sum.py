1fba5bbbd2c17e6e594d32959a36e1b6b9970483
juliettesav <juliettesavarese@gmail.com>
2024-08-20 09:16:21 -0500

    daily commit

--- Grind75/01_two_sum.py
+++ Grind75/01_two_sum.py
@@ -1,3 +1,5 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
 #Pseudo code: 
 
 # Add index 0 and index 1 
--- Grind75/02_valid_parentheses_ANS.py
+++ Grind75/02_valid_parentheses_ANS.py
@@ -1,4 +1,12 @@
-       
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+## 1. Open brackets must be closed by the same type of brackets.
+## 2. Open brackets must be closed in the correct order.
+## 3. Every close bracket has a corresponding open bracket of the same type.
+
+
 def isValid(s: str) -> bool:
     stack = []
     bracket_map = {')': '(', '}': '{', ']': '['}
