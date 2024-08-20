3426ba963fcdd22f44d9b379a83855e1832bde3b
juliettesav <juliettesavarese@gmail.com>
2024-08-20 08:56:26 -0500

    daily commit

--- Grind75/notes.py
+++ Grind75/notes.py
@@ -1,12 +1,1 @@
-def second_largest(list):
-    new_list = sorted(list) 
-    return new_list[-2]
-
-def largest(list):
-    new_list = sorted(list) 
-    return new_list[-1]
-
-print(second_largest([10, 20, 4, 45, 99])) # Output: 45
-print(second_largest([10, 10, 10])) # Output: None
-print(second_largest([1, 8, 2, 15, 99, 28]))
-print(largest([1, 8, 2, 15, 99, 28]))
+# For notes...
