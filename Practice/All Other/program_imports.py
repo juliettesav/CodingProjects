51260b6c0d4711d83ac9308efab3d416b36aff61
juliettesav <juliettesavarese@gmail.com>
2024-08-27 09:17:22 -0500

    daily commit

--- Practice/All Other/program_imports.py
+++ Practice/All Other/program_imports.py
@@ -1,1 +1,6 @@
-import what_are_the_odds
+print("What game would you like to play?")
+print("Press 1 to play 'What are the odds'")
+select = int(input(""))
+
+if select == 1: 
+    import what_are_the_odds
