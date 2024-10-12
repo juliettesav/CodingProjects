79169cbd5cad673f59b561ea968cd599c48bfe53
juliettesav <juliettesavarese@gmail.com>
2024-10-12 12:01:48 -0500

    daily commit

--- Practice/All Other/notes.py
+++ Practice/All Other/notes.py
@@ -14,12 +14,20 @@ if reply == 'y':
     if flavor == "Strawberry":
         print("Strawberries\nWhipped Cream")
     if flavor == "Chocolate": 
-        print("Chocolate Fudge\n Chocolate Sprinkles")
+        print("Chocolate Fudge\nChocolate Sprinkles")
     if flavor == "Vanilla":
         print("Rainbow Sprinkles\nRasberry Syrup")
 
     print("")
-    print("Would you like to add any toppings? (Coming Soon)")
+    print("Would you like to add any toppings?")
+    reply2 = input("Type y for Yes and n for No ")
+
+    if reply2 == "y":
+        input("What kind of toppings would you like? ")
+        print("Cool!")
+    
+    if reply2 == "n":
+        reply = 'n'
 
 if reply == 'n':
     print("No problem! Enjoy your ice cream!")
