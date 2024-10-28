b347f16dcec48c4c676968203f6031a6efaad2be
juliettesav <juliettesavarese@gmail.com>
2024-10-28 10:13:21 -0500

    daily commit

--- Practice/All Other/notes.py
+++ Practice/All Other/notes.py
@@ -1,1 +1,12 @@
-#
+print("Coding is fun!")
+input()
+print("Do you think coding is fun?")
+print()
+ans = input("Enter y for yes or n for no: ")
+print()
+
+if ans == 'y':
+    print("Yeah it is!!!")
+
+if ans == "n":
+    print("Umm well you're wrong.")
